a90d17b322aa57c8e6175f486a5bf4e447276192
Suresh <suresh@Payels-MacBook-Air.local>
2019-08-13 19:19:24 +0530

    added notes from 2 stages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Snowflake Tutorial
-Snowflake Tutorial
+In this tutorial, we would be going through the process of setting Snowflake with sample warehouse. Next, we would be loading a data file onto this warehouse. Last, we would be running sample queries on this warehouse.
 
 ## Create Snowflake user profiles
 
@@ -16,12 +16,20 @@ Here is the script for [creating user profiles](scripts/user_profiles.sql)
 * Verify loaded data 
 * Remove successfully loaded data files 
 
-Here is the script for [loading data into Snowflake](scripts/data_loading.sql)
+Here is the script for [loading data into Snowflake](scripts/data_loading.sql). Here is the snowsql cli command 
+
+```bash
+snowsql -a wg30470.us-east-1 -u etl_user -r anl_write -w etl_write_xs -d sflk_s -s dim	
+```
 
 ## Sample Queries
 
 
 ## Worked Example
 
+
+## FAQ
+
+1. What is the difference between securityadmin and sysadmin roles. Why do we get a sysadmin default.
 
 ## Reference
---
+++ scripts/data_loading.sql
@@ -0,0 +1,38 @@
+-- create table home_sales
+
+use database sflk_s;
+use schema dim;
+
+create or replace table home_sales (
+  city string,
+  zip string,
+  state string,
+  type string default 'Residential',
+  sale_date timestamp_ntz,
+  price string
+  );
+
+
+-- create json file format
+create or replace file format sf_tut_json_format
+  type = 'JSON'
+  strip_outer_array = true;
+
+-- create staging area
+create or replace stage sf_tut_stage
+    file_format = sf_tut_json_format;
+
+-- send local file to staging
+-- put file:///Users/suresh/Projects/snowflake-tutorial/data/sales.json @sf_tut_stage auto_compress=true;
+
+-- look at staging area
+list @sf_tut_stage;
+
+-- copy data into home_sales table
+copy into home_sales(city, state, zip, sale_date, price)
+   from (select substr(parse_json($1):location.state_city,4), substr(parse_json($1):location.state_city,1,2), parse_json($1):location.zip, to_timestamp_ntz(parse_json($1):sale_date), parse_json($1):price
+   from @sf_tut_stage/sf_tut_stage/sales.json.gz t)
+   on_error = 'continue';
+
+-- check relational table
+select * from home_sales;
